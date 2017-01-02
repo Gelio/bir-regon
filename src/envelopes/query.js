5a15310543cc6619478bf9cdf8997c3dc4970a14
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-01-02 16:55:36 +0100

    fix(envelope): add missing property

--- src/envelopes/query.js
+++ src/envelopes/query.js
@@ -1,5 +1,5 @@
 const queryEnvelope = `
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ns="http://CIS/BIR/PUBL/2014/07">
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ns="http://CIS/BIR/PUBL/2014/07" xmlns:dat="http://CIS/BIR/PUBL/2014/07/DataContract">
 <soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
 <wsa:To>{{ DESTINATION_URL }}</wsa:To>
 <wsa:Action>http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/DaneSzukaj</wsa:Action>
